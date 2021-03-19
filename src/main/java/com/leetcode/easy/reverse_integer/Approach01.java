1543b3c36fc7ba86911822228179c7f5576a27cd
LDD이지웅 <jiwoong.lee@kurlycorp.com>
2021-03-20 00:51:52 +0900

    reverse_integer

--- src/main/java/com/leetcode/easy/reverse_integer/Approach01.java
+++ src/main/java/com/leetcode/easy/reverse_integer/Approach01.java
@@ -4,23 +4,18 @@ package com.leetcode.easy.reverse_integer;
 public class Approach01 {
   public static void main(String[] args) {
     long start = System.currentTimeMillis();
-    reverse(123);
+    reverse(-123);
     long end = System.currentTimeMillis();
 
     System.out.println("실행 시간 : " + (end - start) / 1000.0 + "초"); //최종 실행시간
   }
 
   static int reverse(int x) {
-    int rev = 0;
-    while (x != 0) {
-      int pop = x % 10;
-      x /= 10;
-      if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
-      if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
-      rev = rev * 10 + pop;
+    String reversed = new StringBuilder().append(Math.abs(x)).reverse().toString();
+    try {
+      return (x < 0) ? Integer.parseInt(reversed) * -1 : Integer.parseInt(reversed);
+    } catch (NumberFormatException e) {
+      return 0;
     }
-
-    System.out.println("result: " + rev);
-    return rev;
   }
 }
